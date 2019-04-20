ef31149434ddc0796a9b789097d6d4bda72abdfa
hyuna <dlgusdk419@naver.com>
2019-04-20 12:02:38 +0900

    feat(utils): dates, enums

---
+++ calendar-frontend/src/utils/dates.js
@@ -0,0 +1,37 @@
+/**
+ * day count in month
+ * @param year: full year
+ * @param month: month index
+ * @returns {number}
+ */
+function daysInMonth(year, month) {
+  return 32 - new Date(year, month, 32).getDate()
+}
+
+/**
+ * first day index in month
+ * @param year: full year
+ * @param month: month index
+ * @returns {number}
+ */
+function firstDayInMonth(year, month) {
+  return new Date(year, month, 1).getDay()
+}
+
+/**
+ * day count in before month
+ * @param year: full year
+ * @param month: current month index
+ * @returns {number}
+ */
+function beforeDaysInMonth(year, month) {
+  let today = new Date(year, month);
+  today.setMonth(today.getMonth() - 1);
+  return daysInMonth(today.getFullYear(), today.getMonth());
+}
+
+export {
+  daysInMonth,
+  firstDayInMonth,
+  beforeDaysInMonth
+}
